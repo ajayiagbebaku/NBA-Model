400c7337f01ba3e29180119fb1bec7fa7143e670
Ajayi Agbebaku Jr <ajayi1987@me.com>
2021-11-07 07:49:17 -0600

    update projections (11/7/2021)

--- nbamodel.py
+++ nbamodel.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy
 st.set_page_config(page_title="NBA",layout="wide")
 st.header("Today's Predictions 11/6/2021:")
-st.text("Spread Win/Loss: (9-14)")
-st.text("Over/Under Win Loss: (16-9)")
+st.text("Spread Win/Loss: (15-14)")
+st.text("Over/Under Win Loss: (19-12)")
 df = pd.read_excel("UpdatedResults.xlsx")
 st.dataframe(df)
