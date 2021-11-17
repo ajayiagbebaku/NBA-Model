29752de3165e631a86f0f2cc6aac396c2057f0dd
ajayi1987 <ajayi1987@me.com>
2021-11-17 13:02:38 -0600

    update proj.. added injuries in the projection model

--- nbamodel.py
+++ nbamodel.py
@@ -2,14 +2,14 @@ import pandas as pd
 import streamlit as st
 import numpy
 st.set_page_config(page_title="NBA",layout="wide")
-st.header("Today's Predictions 11/16/2021:")
-st.text("Spread Win/Loss: (41-58).414")
-st.text("Over/Under Win Loss: (46-56).451")
-st.text("Spread HIGH Confidence:(2-4)")
-st.text("Spread LOW Confidence:(2-3)")
-st.text("Totals HIGH Confidence:(2-3")
+st.header("Today's Predictions 11/17/2021:")
+st.text("Spread Win/Loss: (43-59).422")
+st.text("Over/Under Win Loss: (48-57).457")
+st.text("Spread HIGH Confidence:(3-4)")
+st.text("Spread LOW Confidence:(3-4)")
+st.text("Totals HIGH Confidence:(4-4")
 st.text("Totals LOW Confidence:(1-5)")
-st.text("Please note projections are made around 9am CST each day")
+st.text("Please note projections are made around 12pm CST each day")
 df_results = pd.read_excel("UpdatedResults.xlsx")
 
 st.dataframe(df_results)
