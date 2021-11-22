f51df79384a8745ab45fd421c77c49cff05da5e4
ajayi1987 <ajayi1987@me.com>
2021-11-22 10:44:58 -0600

    update for 11/22/2021
    added "no bets" with games that are close with my spreads

--- nbamodel.py
+++ nbamodel.py
@@ -2,13 +2,13 @@ import pandas as pd
 import streamlit as st
 import numpy
 st.set_page_config(page_title="NBA",layout="wide")
-st.header("Today's Predictions 11/21/2021:")
-st.text("Spread Win/Loss: (59-78).431")
-st.text("Over/Under Win Loss: (66-74).481")
-st.text("Spread HIGH Confidence:(9-11).450")
-st.text("Spread LOW Confidence:(13-16).448")
-st.text("Totals HIGH Confidence:(11-8).579")
-st.text("Totals LOW Confidence:(12-17).414")
+st.header("Today's Predictions 11/22/2021:")
+st.text("Spread Win/Loss: (62-80).437")
+st.text("Over/Under Win Loss: (69-76).476")
+st.text("Spread HIGH Confidence:(11-12).478")
+st.text("Spread LOW Confidence:(14-17).452")
+st.text("Totals HIGH Confidence:(13-8).619")
+st.text("Totals LOW Confidence:(13-19).414")
 st.text("Please note projections are made around 12pm CST each day")
 df_results = pd.read_excel("UpdatedResults.xlsx")
 
