e4030ad5152f5ebca2677c8140273a395974d9ac
ajayi1987 <ajayi1987@me.com>
2021-11-27 13:04:55 -0600

    update 11/27/21

--- nbamodel.py
+++ nbamodel.py
@@ -2,13 +2,13 @@ import pandas as pd
 import streamlit as st
 import numpy
 st.set_page_config(page_title="NBA",layout="wide")
-st.header("Today's Predictions 11/26/2021:")
-st.text("Spread Win/Loss: (66-87).431")
-st.text("Over/Under Win Loss: (73-86).459")
-st.text("Spread HIGH Confidence:(13-17).433")
-st.text("Spread LOW Confidence:(16-18).471")
-st.text("Totals HIGH Confidence:(16-11).593")
-st.text("Totals LOW Confidence:(15-25).375")
+st.header("Today's Predictions 11/27/2021:")
+st.text("Spread Win/Loss: (72-89).447")
+st.text("Over/Under Win Loss: (79-89).470")
+st.text("Spread HIGH Confidence:(14-18).438")
+st.text("Spread LOW Confidence:(21-19).525")
+st.text("Totals HIGH Confidence:(20-13).606")
+st.text("Totals LOW Confidence:(17-26).395")
 st.text("Please note projections are made around 12pm CST each day")
 df_results = pd.read_excel("UpdatedResults.xlsx")
 
