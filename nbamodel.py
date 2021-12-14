6c1fea953660d62f5a5f81730bae1efb7f0837e9
ajayi1987 <ajayi1987@me.com>
2021-12-14 16:12:39 -0600

    12/14/21

--- nbamodel.py
+++ nbamodel.py
@@ -2,9 +2,9 @@ import pandas as pd
 import streamlit as st
 import numpy
 st.set_page_config(page_title="NBA",layout="wide")
-st.header("Today's Predictions 12/13/2021:")
-st.text("Spread Win/Loss: (101-110).479")
-st.text("Over/Under Win Loss: (110-128).462")
+st.header("Today's Predictions 12/14/2021:")
+st.text("Spread Win/Loss: (103-114).475")
+st.text("Over/Under Win Loss: (115-132).466")
 st.text("Spread HIGH Confidence:(35-29).547")
 st.text("Spread LOW Confidence:(29-30).492")
 st.text("Totals HIGH Confidence:(41-37).526")
